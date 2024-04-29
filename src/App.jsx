83ff87304ae1c6a7568a7c67d6abc27ec96929b9
Ekaterina-Titareva <ekaterina.titareva.ca@gmail.com>
2024-04-30 00:46:07 +0300

    corrected

--- src/App.jsx
+++ src/App.jsx
@@ -14,17 +14,15 @@ function App() {
   return (
     <>
     <header className='header'>
-      <h1>Фэнил hэм Земфира</h1>
+      <h1>Фэнил<br/>hэм<br/>Земфира</h1>
+      <h2>Хормэтле туганнар hэм якыннар!</h2>
       <h2>08.06.2024</h2>
-      <h3>Хормэтле туганнар hэм якыннар!
-        <br />Сезне никах-туебызнын кадерле кунагы булырга чакырабыз!
-      </h3>
+      <h3>Сезне никах-туебызнын кадерле кунагы булырга чакырабыз!</h3>
     </header>
     <main>
       <section id='timetable'>
-          <h2>Көн программасы</h2>
-        {/* <Countdown date={Date.now() + (new Date("2024-06-08")-Date.now())} /> */}
-        <h3>Шимбә, 8 июнь 2024</h3>
+          <h2 className='blockTitle'>Кон программасы</h2>
+        <h3 className='secondTitle'>Шимбә, 8 июнь 2024</h3>
         <div className='timetable'>
           <div className='pointWrapper'>
             <img className='img' src="./mark.svg" alt="Сбор гостей" />
@@ -59,42 +57,46 @@ function App() {
         </div>
       </section>
       <section>
-        <h2>Урынлашу</h2>
+        <h2 className='blockTitle'>Урынлашу</h2>
         <div className='map'>
-        <h3>Ак мәчет, Болгар</h3>
-        <p className='locText'>
-        Безнең тантана Ак мәчеттә Көл Гали урамы, 1, Болгар адресы буенча узачак.
-        </p>
-        <div className='imgWrapper'>
-          <img className='imgLoc' src='./akmechet.jpeg' alt='Белая мечеть'/>
-        </div>
-          <YMaps>
-            <Map defaultState={defaultState}>
-              <Placemark geometry={[54.976473, 49.035627]} />
-            </Map>
-          </YMaps>
+          <h3 className='secondTitle'>Ак мәчет, Болгар</h3>
+          <p className='locText'>
+          Безнең тантана Ак мәчеттә Кол Гали урамы, 1, Болгар адресы буенча узачак.
+          </p>
+          <div className='imgWrapper'>
+            <img className='imgLoc' src='./akmechet.jpeg' alt='Белая мечеть'/>
+          </div>
+          <div className='mapWrapper'>
+            <YMaps className='mapWrapper'>
+              <Map defaultState={defaultState}>
+                <Placemark geometry={[54.976473, 49.035627]} />
+              </Map>
+            </YMaps>
+          </div>
         </div>
         <div className='map'>
-        <h3>Kol Gali Resort&SPA</h3>
-        <p className='locText'>
-          Безнең мәҗлес Kol Gali Resort&SPA ресторанында узачак: Прибрежная урамы, 1, Волга буе Бистәсе.
-        </p>
-        <div className='imgWrapper'>
-          <img className='imgLoc' src='./rest.webp' alt='Ресторан'/>
-        </div>
-          <YMaps>
-            <Map defaultState={defaultStateSec}>
-              <Placemark geometry={[54.990303, 49.087953]} />
-            </Map>
-          </YMaps>
+          <h3 className='secondTitle'>Kol Gali Resort&SPA</h3>
+          <p className='locText'>
+            Безнең мәҗлес Kol Gali Resort&SPA ресторанында узачак: Прибрежная урамы, 1, Волга буе Бистәсе.
+          </p>
+          <div className='imgWrapper'>
+            <img className='imgLoc' src='./rest.jpg' alt='Ресторан'/>
+          </div>
+          <div className='mapWrapper'>
+            <YMaps>
+              <Map defaultState={defaultStateSec}>
+                <Placemark geometry={[54.990303, 49.087953]} />
+              </Map>
+            </YMaps>
+          </div>
         </div>
       </section>
     </main>
     <footer className='footer'>
-      <h2>Без сезне көтеп калабыз!</h2>
-      <div className='imgWrapper'>
-        <img className='imgLoc' src='./couple.jpg' alt='Мы будем вас ждать'/>
-      </div>
+      <h2 className='blockTitle'>Без сезне котеп калабыз!</h2>
+      {/* <div className='imgWrapper'>
+        <img className='imgLoc' src='./footer.jpg' alt='Мы будем вас ждать'/>
+      </div> */}
     </footer>
   </>
   )
